173efc2036dcc7512c765b84a53179dcc36b4c2d
HiralParmar <parmarhiralr@gmail.com>
2017-08-10 14:05:06 -0700

    Added custom validator for date validation

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -20,8 +20,24 @@ export class MediaItemFormComponent{
                 Validators.pattern('[\\w\\-\\s\\/]+')
             ])),
             category: new FormControl(''),
-            year: new FormControl(''),
+            year: new FormControl('',this.yearValidator),
         })
+    }
+
+    yearValidator(control){
+        if(control.value.trim().lenght === 0){
+            return null;
+        }
+
+        let year = parseInt(control.value);
+        let minYear = 1900;
+        let maxYear = 2100;
+        if(year >=minYear && year <= maxYear){
+            return null;
+        }
+        else {
+            return {'year': true};
+        }
     }
     onSubmit(mediaItem){
         console.log(mediaItem);
