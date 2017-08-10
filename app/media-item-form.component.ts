483c5a54848d6bd01f475a591daeec954346c1b9
HiralParmar <parmarhiralr@gmail.com>
2017-08-10 13:40:40 -0700

    Implemented Form using Model driven approach

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'mw-media-item-form',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 
 export class MediaItemFormComponent{
+	form;
+
+    ngOnInit() {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+        this.form = new FormGroup({
+            medium: new FormControl('Movies'),
+            name: new FormControl('',Validators.pattern('[\\w\\-\\s\\/]+')),
+            category: new FormControl(''),
+            year: new FormControl(''),
+        })
+
+    }
+
     onSubmit(mediaItem){
         console.log(mediaItem);
     }
